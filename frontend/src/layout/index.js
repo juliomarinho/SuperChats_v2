de778bc331edaa64bc2f2ea9289a935bbd9dccbb
juliomarinho <julio.m4r1nh0@gmail.com>
2023-06-03 10:49:35 -0300

    SuperChats_v2
    SuperChats_v2

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -267,7 +267,7 @@ const LoggedInLayout = ({ children }) => {
           >
             {greaterThenSm ? (
               <>
-                Olá, <b>{user.name}</b>! Seja bem vindo ao OneZape! 🤍💙
+                Olá, <b>{user.name}</b>! Seja bem vindo ao SuperChats! 🤍💙
               </>
             ) : (
               user.name
